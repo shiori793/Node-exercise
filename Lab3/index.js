54abd7eb9a042ecc10fe15892ae8520e2eed82c9
Shiori <shiori.fujii.cb@gmail.com>
2023-06-04 12:37:56 -0700

    Lab3

---
+++ Lab3/index.js
@@ -0,0 +1,112 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+const users = [];
+
+const secretKey = 'my-secret-key';
+
+async function hashedPassword(password){
+    const saltRounds = 10;
+    const hashed = await bcrypt.hash(password, saltRounds);
+    return hashed;
+}
+
+async function comparePasswords(password, hashedPassword) {
+    const match = await bcrypt.compare(password, hashedPassword);
+    return match
+}
+
+function generateToken(payload) {
+    return jwt.sign(payload, secretKey, { expiresIn: "1h" })
+}
+
+function verifyToken(token) {
+    return jwt.verify(token, secretKey);
+}
+
+function authenticateToken(req, res, next) {
+    const { authorization } = req.headers;
+    if (!authorization) {
+        return res.status(401).json({
+            message: "Unauthorized"
+        })
+    }
+    const user = verifyToken(authorization);
+
+    if (!user) {
+        return res.status(401).json({
+            message: "Unauthorized"
+        })
+    }
+
+    req.user = user;
+    next();
+}
+
+app.post("/register", async (req, res) => {
+    const { username, password } = req.body;
+
+    let existingUser = users.find(user => user.username === username);
+
+    if(existingUser){
+        res.status(409).json({
+            message: "Username already exist"
+        })
+    }
+
+    const hashed = await hashedPassword(password);
+
+    const newUser = {
+        id: users.length + 1,
+        username,
+        password: hashed
+    }
+
+    users.push(newUser);
+
+    res.status(201).json({
+        message: "user registered successfully!"
+    })
+
+})
+
+app.post("/login", async (req, res) => {
+    const { username, password } = req.body;
+
+    let user = users.find(user => user.username === username);
+
+    if(!user) {
+        res.status(401).json({
+            message: "Invalid credentials!"
+        })
+    }
+
+    const match = await comparePasswords(password, user.password);
+
+    if(!match) {
+        res.status(401).json({
+            message: "Wrong password!"
+        })
+    }
+
+    const token = generateToken({ id: user.id, username: user.username });
+
+    res.status(200).json({
+        token
+    })
+
+})
+
+app.get("/protected", authenticateToken, (req, res) => {
+    res.json({
+        message: "Hello World"
+    })
+})
+
+app.listen(3001, () => {
+    console.log("Server is running on 3001");
+})
--- boilerplate-express/myApp.js
+++ boilerplate-express/myApp.js
@@ -1,14 +1,61 @@
 let express = require('express');
 let app = express();
+let bodyParser = require('body-parser');
+require('dotenv').config();
 
+app.use('/public', express.static(__dirname + '/public'))
+app.use(bodyParser.urlencoded({extended: false}))
 
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.path} - ${req.ip}`)
+    next()
+})
 
+// console.log('Hello World')
 
+// app.get('/', (req, res) => {
+//   res.send('Hello Express')
+// })
 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html')
+})
 
+app.get('/json', (req, res) => {
+    res.json({
+        "message": process.env.MESSAGE_STYLE === 'uppercase' ? "HELLO JSON" : "Hello json"
+    })
+})
 
+app.get('/now', (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+  },(req, res) => {
+    res.send({
+      time: req.time
+    })
+  })
 
+  app.get('/:word/echo', (req, res) => {
+    const word = req.params.word;
+    res.send({
+      echo: word
+    })
+  })
 
+  app.get('/name', (req, res) => {
+    const { first, last } = req.query
+    res.send({ 
+      name: `${first} ${last}`
+    })
+  })
+
+  app.post('/name', (req, res) => {
+    const { first, last } = req.body
+    res.send({ 
+      name: `${first} ${last}`
+    })
+  })
 
 
 
